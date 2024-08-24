1ae79bc64bb1b9372898c6fade9bdf6f41c3b1b6
Oliver Wieland <oliverwieland@Olivers-MacBook-Pro.local>
2024-08-24 17:25:05 +0100

    [SD013] Tidied up script3. Deleted script2. Script 3 might be ready to ship.

--- script3.js
+++ script3.js
@@ -1,28 +1,28 @@
-function article() {
-    const postContentContainer = document.getElementById('post-content'); //Ggets the container div for the article.
-    if (postContentContainer === null) {
-      console.log("The current page does not have the required container `div` with an ID of `post-content`. Ergo this script will not do much work.", postContentContainer);
-      return;
-    }
-    var articleEl = postContentContainer.querySelectorAll('article')[0]; // Gets the first instance of an article element
-    if (articleEl === null) {
-      console.log("The current page does not have the required `article` element. Ergo this script will not do much work.");
-      return;
-    }
-    return articleEl;
-  };
+function getTheArticle() {
+  const postContentContainer = document.getElementById('post-content');
+  if (postContentContainer === null) {
+    console.log("The current page does not have the required container `div` with an ID of `post-content`. Ergo this script will not do much work.", postContentContainer);
+    return;
+  }
+  var firstArticleElement = postContentContainer.querySelectorAll('article')[0];
+  if (firstArticleElement === null) {
+    console.log("The current page does not have the required `article` element. Ergo this script will not do much work.");
+    return;
+  }
+  return firstArticleElement;
+};
   
-  const articleLength = article().scrollHeight; //ex: 2000px. Not directly used.
+const articleLength = getTheArticle().scrollHeight;
   
-const checkpoints = [
-    { message: "0% to 25% read!", location: article().offsetTop },
-    { message: "25% to 50% read!", location: articleLength * 0.25 + article().offsetTop },
-    { message: "50% to 75% read!", location: articleLength * 0.5 + article().offsetTop },
-    { message: "75% to 100% read!", location: articleLength * 0.75 + article().offsetTop },
-    { message: "100% read!", location: articleLength + article().offsetTop }
+const offsets = [
+  { percentage: "0%", message: "0% to 25% read!", location: getTheArticle().offsetTop },
+  { percentage: "25%", message: "25% to 50% read!", location: articleLength * 0.25 + getTheArticle().offsetTop },
+  { percentage: "50%", message: "50% to 75% read!", location: articleLength * 0.5 + getTheArticle().offsetTop },
+  { percentage: "75%", message: "75% to 100% read!", location: articleLength * 0.75 + getTheArticle().offsetTop },
+  { percentage: "100%", message: "100% read!", location: articleLength + getTheArticle().offsetTop }
 ];
 
-// Define the debounce function (adopted from Underscore.js)
+// Debounce function (from Underscore.js)
 const debounce = (func, wait, immediate) => {
   var timeout;
   return function () {
@@ -35,32 +35,32 @@ const debounce = (func, wait, immediate) => {
   };
 };
 
-function checkpointEvents(loc, mes) {
-    return new CustomEvent("checkpoint", {detail: {location: loc, message: mes}});
+function checkpointEvents(per) {
+    return new CustomEvent("checkpoint", {detail: {percentage: per}});
 };
 
 function dispatch(num) {
-  window.dispatchEvent(checkpointEvents(checkpoints[num].location, checkpoints[num].message));
-  alert(checkpoints[num].message + " Past the " + checkpoints[num].location + "px checkpoint.");
-  console.log("Dispatched: ", checkpointEvents(checkpoints[num].location, checkpoints[num].message));
+  window.dispatchEvent(checkpointEvents(offsets[num].percentage)); // <- this is really it
+  alert(offsets[num].message + " Past the " + offsets[num].location + "px checkpoint."); // <- to let the user know
+  console.log("Dispatched: ", checkpointEvents(offsets[num].percentage)); // <- to show what was dispatched
 };
 
-// Check scroll depth through article and fire events at checkpoints
+// Check scroll depth through article and fire events at offsets
 window.addEventListener("scroll", debounce(() => {
   checkScrollDepth();
 }, 1500));
 
 function checkScrollDepth() {
   const scrollY = document.documentElement.scrollTop;
-  if (scrollY >= checkpoints[4].location) {
+  if (scrollY >= offsets[4].location) {
     dispatch(4);
-  } else if (scrollY >= checkpoints[3].location) {
+  } else if (scrollY >= offsets[3].location) {
     dispatch(3);
-  } else if (scrollY >= checkpoints[2].location) {
+  } else if (scrollY >= offsets[2].location) {
     dispatch(2);
-  } else if (scrollY >= checkpoints[1].location) {
+  } else if (scrollY >= offsets[1].location) {
     dispatch(1);
-  } else if (scrollY >= checkpoints[0].location) {
+  } else if (scrollY >= offsets[0].location) {
     dispatch(0);
   }
   // else if not even begun reading
