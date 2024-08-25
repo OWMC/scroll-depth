dc9674f85f24c6899af4f5fbfb5167e8511a6322
Oliver Wieland <oliverwieland@Olivers-MacBook-Pro.local>
2024-08-25 15:58:00 +0100

    [SD013] Bug and debounce function type.

--- script3.js
+++ script3.js
@@ -19,45 +19,45 @@ document.addEventListener("DOMContentLoaded", () => {
         zeroPercentCheckpoint: { percentage: "0%", message: "0% to 25% read!", location: getTheArticle().offsetTop },
         twentyFivePercentCheckpoint: { percentage: "25%", message: "25% to 50% read!", location: articleLength * 0.25 + getTheArticle().offsetTop },
         fiftyPercentCheckpoint: { percentage: "50%", message: "50% to 75% read!", location: articleLength * 0.5 + getTheArticle().offsetTop },
-        seentyFivePercentCheckpoint: { percentage: "75%", message: "75% to 100% read!", location: articleLength * 0.75 + getTheArticle().offsetTop },
+        seventyFivePercentCheckpoint: { percentage: "75%", message: "75% to 100% read!", location: articleLength * 0.75 + getTheArticle().offsetTop },
         hundredPercentCheckpoint: { percentage: "100%", message: "100% read!", location: articleLength + getTheArticle().offsetTop }
     };
 
     // Debounce function (from Underscore.js)
-    const debounce = (func, wait, immediate) => {
-        var timeout;
-        return function () {
+    function debounce(func, wait) {
+        var timeout = null;
+        return function() {
             var context = this;
             var args = arguments;
             clearTimeout(timeout);
-            timeout = setTimeout(function () {
-            if (!immediate) func.apply(context, args);
+            timeout = setTimeout(function() {
+                func.apply(context, args);
             }, wait);
         };
     };
-
-    function checkpointEvents(percentage) {
+      
+    function offsetEvent(percentage) {
         return new CustomEvent("checkpoint", {detail: {percentage: percentage}});
     };
 
     function dispatchScrollOffset(offset) {
-        window.dispatchEvent(checkpointEvents(offset.percentage)); // <- this is really it
-        alert(offset.message + " Past the " + offset.location + "px checkpoint."); // <- to let the user know
-        console.log("Dispatched: ", checkpointEvents(offset.percentage)); // <- to show what was dispatched
+        window.dispatchEvent(offsetEvent(offset.percentage)); // <- this is really it
+        console.log(offset.message + " Past the " + offset.location + "px checkpoint."); // <- to let the user know
+        console.log("Dispatched: ", offsetEvent(offset.percentage).detail); // <- to show what was dispatched
     };
 
     function handleScroll() {
         const userScrollLocation = document.documentElement.scrollTop;
-        if (userScrollLocation >= offsets.zeroPercentCheckpoint.location) {
-            dispatchScrollOffset(offsets.zeroPercentCheckpoint);
-        } else if (userScrollLocation >= offsets.twentyFivePercentCheckpoint.location) {
-            dispatchScrollOffset(twentyFivePercentCheckpoint);
+        if (userScrollLocation >= offsets.hundredPercentCheckpoint.location) {
+            dispatchScrollOffset(offsets.hundredPercentCheckpoint);
+        } else if (userScrollLocation >= offsets.seventyFivePercentCheckpoint.location) {
+            dispatchScrollOffset(offsets.seventyFivePercentCheckpoint);
         } else if (userScrollLocation >= offsets.fiftyPercentCheckpoint.location) {
-            dispatchScrollOffset(fiftyPercentCheckpoint);
-        } else if (userScrollLocation >= offsets.seentyFivePercentCheckpoint.location) {
-            dispatchScrollOffset(seentyFivePercentCheckpoint);
-        } else if (userScrollLocation >= offsets.hundredPercentCheckpoint.location) {
-            dispatchScrollOffset(hundredPercentCheckpoint);
+            dispatchScrollOffset(offsets.fiftyPercentCheckpoint);
+        } else if (userScrollLocation >= offsets.twentyFivePercentCheckpoint.location) {
+            dispatchScrollOffset(offsets.twentyFivePercentCheckpoint);
+        } else if (userScrollLocation >= offsets.zeroPercentCheckpoint.location) {
+            dispatchScrollOffset(offsets.zeroPercentCheckpoint);
         }
         // else if not even begun reading
     };
@@ -66,5 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", debounce(() => {
         handleScroll();
     }, 1500));
-
 });
