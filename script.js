a922c2066fba88b89eb1d1de9b23e0bdfd762a4f
Oliver Wieland <oliverwieland@Olivers-MacBook-Pro.local>
2024-08-26 22:36:07 +0100

    [SD015] Pure function to replace offsets object.

--- script.js
+++ script.js
@@ -17,31 +17,33 @@ function getTheArticle() {
 };
 
 const articleLength = getTheArticle().scrollHeight;
+const articleLocation = getTheArticle().offsetTop;
 
-const offsets = {
-  twentyFivePercentCheckpoint: { percentage: "25%", message: "Passed the 25% checkpoint of this article!", location: articleLength * 0.25 + getTheArticle().offsetTop },
-  fiftyPercentCheckpoint: { percentage: "50%", message: "Passed the 50% checkpoint of this article!", location: articleLength * 0.5 + getTheArticle().offsetTop },
-  hundredPercentCheckpoint: { percentage: "100%", message: "Passed the 100% checkpoint of this article!", location: articleLength + getTheArticle().offsetTop }
+function getOffset(percentage, elementLength, headerGap) {
+  return {
+    checkpoint: { message: `Passed the ${percentage}% checkpoint of this article!`, location: elementLength * percentage / 100 + headerGap },
+  }
 };
 
 function offsetEvent(percentage) {
-  return new CustomEvent("checkpoint", {detail: {percentage: percentage}});
+  return new CustomEvent("checkpoint", {detail: {percentageScrolled: percentage}});
 };
 
 function dispatchScrollOffset(offset) {
-  window.dispatchEvent(offsetEvent(offset.percentage)); // <- this is really it
-  alert(offset.message + " (" + offset.location + "px )"); // <- to let the user know
-  console.log("Dispatched: ", offsetEvent(offset.percentage).detail); // <- to show what was dispatched
+  window.dispatchEvent(offsetEvent(offset)); // <- this is really it
+  alert(getOffset(offset, articleLength, articleLocation).checkpoint.message + " (" + getOffset(offset, articleLength, articleLocation).checkpoint.location + "px )"); // <- to let the user know
+  console.log("Dispatched: ", offsetEvent(offset).detail); // <- to show what was dispatched
 };
+
 
 function handleScroll() {
   const userScrollLocation = document.documentElement.scrollTop;
-  if (userScrollLocation >= offsets.hundredPercentCheckpoint.location) {
-      dispatchScrollOffset(offsets.hundredPercentCheckpoint);
-  } else if (userScrollLocation >= offsets.fiftyPercentCheckpoint.location) {
-      dispatchScrollOffset(offsets.fiftyPercentCheckpoint);
-  } else if (userScrollLocation >= offsets.twentyFivePercentCheckpoint.location) {
-      dispatchScrollOffset(offsets.twentyFivePercentCheckpoint);
+  if (userScrollLocation >= getOffset(100, articleLength, articleLocation).checkpoint.location) {
+      dispatchScrollOffset(100);
+  } else if (userScrollLocation >= getOffset(50, articleLength, articleLocation).checkpoint.location) {
+      dispatchScrollOffset(50);
+  } else if (userScrollLocation >= getOffset(25, articleLength, articleLocation).checkpoint.location) {
+      dispatchScrollOffset(25);
   };
 };
 
