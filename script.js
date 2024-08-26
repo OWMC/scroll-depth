6c921e520c1d7f98cf8b09d67abe0f0d1fe163f7
Oliver Wieland <oliverwieland@Olivers-MacBook-Pro.local>
2024-08-26 23:50:21 +0100

    [SD015] Better fallback if markup is wrong.

--- script.js
+++ script.js
@@ -5,57 +5,59 @@
 function getTheArticle() {
   const postContentContainer = document.getElementById('post-content');
   if (postContentContainer === null) {
-      console.log("The current page does not have the required container `div` with an ID of `post-content`. Ergo this script will not do much work.", postContentContainer);
       return;
   }
   const firstArticleElement = postContentContainer.querySelectorAll('article')[0];
   if (firstArticleElement === null) {
-      console.log("The current page does not have the required `article` element. Ergo this script will not do much work.");
       return;
   }
   return firstArticleElement;
 };
 
-const articleLength = getTheArticle().scrollHeight;
-const articleLocation = getTheArticle().offsetTop;
+if (getTheArticle() !== undefined) { 
+  const articleLength = getTheArticle().scrollHeight;
+  const articleLocation = getTheArticle().offsetTop;
 
-function offsetEvent(percentage) {
-  return new CustomEvent("checkpoint", {detail: {percentageScrolled: percentage}});
-};
-
-function dispatchScrollOffset(percentage) {
-  window.dispatchEvent(offsetEvent(percentage)); // <- this is really it
-  console.log("Dispatched: ", offsetEvent(percentage).detail); // <- to show what was dispatched
-};
-
-function getLocation(percentage, elementLength, headerGap) {
-  return elementLength * percentage / 100 + headerGap;
-};
-
-function handleScroll() {
-  const userScrollLocation = document.documentElement.scrollTop;
-  if (userScrollLocation >= getLocation(100, articleLength, articleLocation)) {
-      dispatchScrollOffset(100);
-  } else if (userScrollLocation >= getLocation(50, articleLength, articleLocation)) {
-      dispatchScrollOffset(50);
-  } else if (userScrollLocation >= getLocation(25, articleLength, articleLocation)) {
-      dispatchScrollOffset(25);
+  function offsetEvent(percentage) {
+    return new CustomEvent("checkpoint", {detail: {percentageScrolled: percentage}});
   };
-};
 
-// Debounce function (from Underscore.js)
-function debounce(func, wait) {
-  var timeout = null;
-  return function() {
-      var context = this;
-      clearTimeout(timeout);
-      timeout = setTimeout(function() {
-          func.apply(context, arguments);
-      }, wait);
+  function dispatchScrollOffset(percentage) {
+    window.dispatchEvent(offsetEvent(percentage)); // <- this is really it
+    console.log("Dispatched: ", offsetEvent(percentage).detail); // <- to show what was dispatched
   };
-};
 
-// Check scroll depth through article and fire events at offsets
-window.addEventListener("scroll", debounce(() => {
-  handleScroll();
-}, 1500));
+  function getLocation(percentage, elementLength, headerGap) {
+    return elementLength * percentage / 100 + headerGap;
+  };
+
+  function handleScroll() {
+    const userScrollLocation = document.documentElement.scrollTop;
+    if (userScrollLocation >= getLocation(100, articleLength, articleLocation)) {
+        dispatchScrollOffset(100);
+    } else if (userScrollLocation >= getLocation(50, articleLength, articleLocation)) {
+        dispatchScrollOffset(50);
+    } else if (userScrollLocation >= getLocation(25, articleLength, articleLocation)) {
+        dispatchScrollOffset(25);
+    };
+  };
+
+  // Debounce function (from Underscore.js)
+  function debounce(func, wait) {
+    var timeout = null;
+    return function() {
+        var context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            func.apply(context, arguments);
+        }, wait);
+    };
+  };
+
+  // Check scroll depth through article and fire events at offsets
+  window.addEventListener("scroll", debounce(() => {
+    handleScroll();
+  }, 1500));
+} else {
+  console.log("The current page does not have the required markup (see readme). Ergo this script will not do much apart from tell you this.");
+}
